4bbed636c34a5e751580dd9ae18a2d49fd3f8ba9
Shukri Hussein <husseinshukri@outlook.com>
2024-02-27 12:13:18 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// Write your algorithm here
+// function isPalindrome(word) {
+//   return word === word.split('').reverse().join('');
+  
+// }
+
+/*function isPalindrome(word) {
+  const wordLr = word
+  const wordRl = word.split('').reverse().join('');
+  return word === wordRl
+} */
+
+isPalidrome = (word) => {
+  return === word.split('').reverse('').join('')
 }
 
 /* 
   Add your pseudocode here
+  //write a function for is
+  //is function will receive 1 argument
+  //is returns true if string is a palindrome
+
 */
 
 /*
@@ -11,6 +27,7 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
+
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
